7d26ad38e9bb8ce8dc453985123d6096f1d16492
ckhan <chungkyun.han@gmail.com>
2017-07-27 16:27:14 +0800

    t

--- a_dataProcessing/run.sh
+++ a_dataProcessing/run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 for i in 0{1..9} {10..12}; do
-    python -c "from a1_stateBlockByMonth import run; run('12$i')" &
+    python -c "from a1_stateBlockByMonth import run; run('10$i')" &
 done
 
